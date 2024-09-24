2d5986eef8c7d6f0b8372f34f7824b3d86b73772
lebi raja <130662829+lebiraja@users.noreply.github.com>
2024-09-24 17:50:31 +0530

    Update joint.js

--- joint.js
+++ joint.js
@@ -1,17 +1,16 @@
-// Function to open the modal and display the clicked image
-function openModal(imageElement) {
-    const modal = document.getElementById("imageModal");
-    const modalImage = document.getElementById("modalImage");
-    const caption = document.getElementById("caption");
-  
-    modal.style.display = "block";
-    modalImage.src = imageElement.src; // Set the modal image to the clicked image's source
-    caption.innerHTML = imageElement.alt; // Set the caption to the image's alt text
-  }
-  
-  // Function to close the modal
-  function closeModal() {
-    const modal = document.getElementById("imageModal");
-    modal.style.display = "none"; // Hide the modal
-  }
-  
+// JavaScript to open modal and display selected image
+function openModal(imgElement) {
+  const modal = document.getElementById("imageModal");
+  const modalImg = document.getElementById("modalImage");
+  const captionText = document.getElementById("caption");
+
+  modal.style.display = "block";
+  modalImg.src = imgElement.src;
+  captionText.innerHTML = imgElement.alt;
+}
+
+// JavaScript to close the modal
+function closeModal() {
+  const modal = document.getElementById("imageModal");
+  modal.style.display = "none";
+}
